e5af67d7801a03337dba1aebfd82726aebf61ab6
OSKAR GRZENDA <G00373428@gmit.ie>
2021-03-08 20:48:27 +0000

    Styled the website

--- project-app/src/app/pages/menu/menu.module.ts
+++ project-app/src/app/pages/menu/menu.module.ts
@@ -5,14 +5,29 @@ import { MenuComponent } from './menu.component';
 
 import { MatButtonModule } from '@angular/material/button'
 
+import { MatCardModule } from '@angular/material/card';
+import { MatCommonModule } from '@angular/material/core';
+import { MatInputModule } from '@angular/material/input';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
 
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 
 @NgModule({
   declarations: [MenuComponent],
   imports: [
     CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
     MatButtonModule,
+    MatCardModule,
+    MatCommonModule,
+    MatInputModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatSnackBarModule,
     RouterModule.forChild([{ path: '', component: MenuComponent }])
   ]
 })
