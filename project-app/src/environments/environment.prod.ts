9ee554c350aa179002e859b1e5b90110f6d3c87d
unknown <g00373427@gmit.ie>
2021-04-19 13:51:51 +0100

    Deployed the webapp to the internet

--- project-app/src/environments/environment.prod.ts
+++ project-app/src/environments/environment.prod.ts
@@ -1,4 +1,14 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyAIW2oFje8oX9rYNXrvyMKXFH5eSwofsPU",
+    authDomain: "ppit-25a16.firebaseapp.com",
+    databaseURL: "https://ppit-25a16-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "ppit-25a16",
+    storageBucket: "ppit-25a16.appspot.com",
+    messagingSenderId: "123342587077",
+    appId: "1:123342587077:web:59d7b725fea8dd8be7dcac",
+    measurementId: "G-37VR004F00"
+  }
 };
 
